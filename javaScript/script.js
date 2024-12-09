5b5ab9ac465c41f0ac87049aff39edba1eb5f12d
Bahmo-max <bahms@mail.uc.edu>
2024-12-09 00:15:23 -0500

    Message Bar

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,15 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
+        const message = document.getElementById('message').value;
+
 
         const thankYouMessage = document.getElementById("thankYouMessage");
 
         thankYouMessage.textContent = `Thank you, ${name}! Your email (${email}) has been received.`;
         console.log("Name entered:", name);
         console.log("Email entered:", email);
+        console.log("Message entered :",message);
 
         form.reset();
     });
+});
+
+
+
+document.getElementById('showContactInfo').addEventListener('click', function () {
+    const contactInfo = document.getElementById('contactInfo');
+    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
 });
 
 
